938c8d7fa737b7dbb04ea3bc474d6fddb1862632
Mayeed <mayeedmuhit@gmail.com>
2025-04-16 03:24:08 +0100

    Done

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ from function import get_todos
 todos = function.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] + '\n'
+    todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     function.write_todos(todos)
 
@@ -13,8 +13,14 @@ def add_todo():
 st.title("My To-Do App")
 st.subheader("This is my todo app")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label="", placeholder="Add a new todo..",
               on_change= add_todo, key= 'new_todo')
